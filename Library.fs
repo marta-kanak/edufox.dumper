bde67f547caa13a4267881d6d3a429e538d19187
Marta Dycjan <marta.kanak@gmail.com>
2019-10-24 18:27:25 +0200

    Fix auth. Publish single file.

--- Library.fs
+++ Library.fs
@@ -3,11 +3,13 @@
 module Dumper =
     open System
 
-    printfn "Username:"
+    printfn "========= Hello to Edufox dumper =========\nTo proceed please enter your credentials. We don't store or use your data in any malicious way - just to dump all photos to your local machine.\n\nDon't hesitate to contact us if you have any problems. Enjoy! :)"
+    printfn "===========================================\n"
+    printf "Edufox username:"
     let username = Console.ReadLine()
-    printfn "Password:"
+    printf "\nEdufox password:"
     let password = Console.ReadLine()
-    printfn "Directory path:"
+    printf "\nDirectory path -where to save picture, eg C:\zlobek :"
     let path = Console.ReadLine()
-        
+
     Edufox.processFirstPage username password path |> ignore
